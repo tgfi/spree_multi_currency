343b50ce674db5bf7c3550d461de0add8bba7682
Greg Benedict <gbenedict@tgfi.net>
2013-12-02 11:01:15 -0500

    Fixing rails 4 routing issue.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Spree::Core::Engine.routes.draw do
-  match '/currency/set', :to => 'currency#set', :defaults => { :format => :json }, :as => :set_currency
+  '/currency/set', :to => 'currency#set', :defaults => { :format => :json }, :as => :set_currency, via: [:get, :post]
 
   namespace :admin do
     resources :products do
