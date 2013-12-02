66c914131eb92241040d9131dbc036cd49115e38
Greg Benedict <gbenedict@tgfi.net>
2013-12-02 11:05:52 -0500

    Added the missing match statement back to the routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Spree::Core::Engine.routes.draw do
-  '/currency/set', :to => 'currency#set', :defaults => { :format => :json }, :as => :set_currency, via: [:get, :post]
+  match '/currency/set', :to => 'currency#set', :defaults => { :format => :json }, :as => :set_currency, :via => [:get, :post]
 
   namespace :admin do
     resources :products do
